67137decdf6b9af14a3de0f5b197cbedfdf0fef4
GramingFoxTeam <ibrahimbozkurt0270@gmail.com>
2022-07-02 16:04:57 +0300

    Fix #4, remove undici

--- README.md
+++ README.md
@@ -13,31 +13,37 @@ If you aren't tech savvy, you can download the prebuilt EXE file from [here](htt
 ## How to build
 
 1. Clone or download the repository.
+
 ```bash
 $ git clone https://github.com/reisxd/revanced-builder
 ```
 
 2. Install the modules.
+
 ```bash
 $ npm install
 ```
 
 3. Build ReVanced!
+
 ```bash
 $ node . --patch
 ```
 
 If you want to exclude a patch, use the `--exclude` option:
+
 ```bash
 $ node . --patch --exclude disable-shorts-button
 ```
 
 If you want to exclude more, seperate the patches with a comma:
+
 ```bash
 $ node . --patch --exclude disable-shorts-button,microg-support
 ```
 
 If you want to patch a specific YT version, download the APK, move it to this folder and rename it to `youtube.apk`:
+
 ```bash
 $ node . --patch --manual-apk
 ```
--- index.js
+++ index.js
@@ -40,7 +40,7 @@ process.on('unhandledRejection', async (reason) => {
   await new Promise(() => process.stdin.on('data', () => process.exit()));
 });
 
-async function overWriteJarNames(link) {
+async function overWriteJarNames (link) {
   const fileName = link.split('/').pop();
   // i have to use ifs for this sorry
   if (fileName.includes('revanced-cli')) jarNames.cli += fileName;
@@ -51,7 +51,7 @@ async function overWriteJarNames(link) {
   if (fileName.startsWith('microg')) jarNames.microG += fileName;
 }
 
-async function getDownloadLink(json) {
+async function getDownloadLink (json) {
   const apiRequest = await fetchURL(
     `https://api.github.com/repos/${json.owner}/${json.repo}/releases/latest`
   );
@@ -59,7 +59,7 @@ async function getDownloadLink(json) {
   return jsonResponse.assets;
 }
 
-async function getPage(pageUrl) {
+async function getPage (pageUrl) {
   const pageRequest = await fetchURL(pageUrl, {
     headers: {
       'user-agent':
@@ -69,7 +69,7 @@ async function getPage(pageUrl) {
   return await pageRequest.text();
 }
 
-async function dloadFromURL(url, outputPath) {
+async function dloadFromURL (url, outputPath) {
   const request = await fetchURL(url, {
     headers: {
       'user-agent':
@@ -115,7 +115,7 @@ async function dloadFromURL(url, outputPath) {
   });
 }
 
-async function downloadYTApk(ytVersion) {
+async function downloadYTApk (ytVersion) {
   const versionsList = await getPage(
     'https://www.apkmirror.com/apk/google-inc/youtube'
   );
@@ -154,7 +154,7 @@ async function downloadYTApk(ytVersion) {
   return console.log('Download complete!');
 }
 
-async function downloadFile(assets) {
+async function downloadFile (assets) {
   for (const asset of assets) {
     const dir = fs.readdirSync('./revanced/');
     overWriteJarNames(asset.browser_download_url);
@@ -166,14 +166,14 @@ async function downloadFile(assets) {
   }
 }
 
-async function downloadFiles(repos) {
+async function downloadFiles (repos) {
   for (const repo of repos) {
     const downloadLink = await getDownloadLink(repo);
     await downloadFile(downloadLink);
   }
 }
 
-async function getADBDeviceID() {
+async function getADBDeviceID () {
   let deviceId;
   const { stdout } = await actualExec('adb devices');
   const match = stdout.match(/^(\w+)\s+device$/m);
@@ -187,7 +187,7 @@ async function getADBDeviceID() {
   return deviceId;
 }
 
-async function checkForJavaADB() {
+async function checkForJavaADB () {
   try {
     const javaCheck = await actualExec('java -version');
     const javaVer = Array.from(javaCheck.stderr.matchAll(/version\s([^:]+)/g))
@@ -215,7 +215,7 @@ async function checkForJavaADB() {
   }
 }
 
-async function getYTVersion() {
+async function getYTVersion () {
   const { stdout, stderr } = await actualExec(
     'adb shell dumpsys package com.google.android.youtube'
   );
@@ -370,13 +370,15 @@ async function getYTVersion() {
 
       const patchesArray =
         getPatches.stdout.match(/INFO:\s([^:]+)/g) ||
-        getPatches.stderr.match(/INFO:\s([^:]+)/g);
+        getPatches.stderr.match(/INFO:\s([^:]+)/g) ||
+        getPatches.stdout.match(/INFORMATION:\s([^:]+)/g) ||
+        getPatches.stderr.match(/INFORMATION:\s([^:]+)/g);
 
       const patchesChoice = [];
 
       for (const patchName of patchesArray) {
         patchesChoice.push({
-          name: patchName.replace('INFO: ', '')
+          name: patchName.replace('INFO: ', '').replace('INFORMATION: ', '')
         });
       }
 
