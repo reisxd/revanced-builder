0e87fd4f1ad2edac9a5cdc36e7409dab20cf2a47
reis <reis@turkeylist.gq>
2022-08-03 14:37:06 +0300

    Fix mount script

--- utils/mountReVanced.js
+++ utils/mountReVanced.js
@@ -13,7 +13,7 @@ export default async function (pkg, ws) {
   await actualExec('su -c "mkdir -p /data/adb/revanced/"');
   // Move APK to folder
   await actualExec(
-    `su -c "base_path=\"/data/adb/revanced/${pkg}.apk\" && mv /data/local/tmp/revanced.delete $base_path && chmod 644 $base_path && chown system:system $base_path && chcon u:object_r:apk_data_file:s0  $base_path"`
+    `su -c 'base_path=\"/data/adb/revanced/${pkg}.apk\" && mv /data/local/tmp/revanced.delete $base_path && chmod 644 $base_path && chown system:system $base_path && chcon u:object_r:apk_data_file:s0  $base_path'`
   );
   // Create Mount script
   fs.writeFileSync(
