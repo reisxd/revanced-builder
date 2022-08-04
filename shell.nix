6f45170ff6524a6435c102006c2ae6cfd33dc001
Fernando Ayats <ayatsfer@gmail.com>
2022-08-04 15:54:03 +0200

    nix: change to simpler shell and change to headless java

---
+++ shell.nix
@@ -0,0 +1,11 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShellNoCC {
+    name = "revanced-builder";
+    packages = [
+      pkgs.android-tools
+      pkgs.nodejs
+      pkgs.jdk17_headless
+    ];
+  }
