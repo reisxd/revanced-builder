7526279b68991397fb7f2c6a08b1375f5aac2141
GramingFoxTeam <ibrahimbozkurt0270@gmail.com>
2022-08-26 14:41:06 +0300

    v3.3.2

--- wsEvents/CheckForUpdates.js
+++ wsEvents/CheckForUpdates.js
@@ -5,7 +5,7 @@ module.exports = async function (message, ws) {
     await getDownloadLink({ owner: 'reisxd', repo: 'revanced-builder' })
   ).version;
   // Well, there's no other good way to do this, so...
-  const currentVersion = 'v3.3.1';
+  const currentVersion = 'v3.3.2';
 
   if (builderVersion !== currentVersion) {
     return ws.send(
