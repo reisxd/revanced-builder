3ff2ae76450c81f79adf71a1ac89a1a3f4446d70
GramingFoxTeam <reis@turkeylist.gq>
2022-09-04 17:01:10 +0300

    v3.4.1

--- wsEvents/checkForUpdates.js
+++ wsEvents/checkForUpdates.js
@@ -1,6 +1,6 @@
 const { getDownloadLink } = require('../utils/FileDownloader.js');
 
-const currentVersion = 'v3.3.7';
+const currentVersion = 'v3.4.1';
 
 /**
  * @param {import('ws').WebSocket} ws
