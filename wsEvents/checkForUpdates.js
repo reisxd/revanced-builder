dad30f3e86a88100abf84188359c995e6f76844c
shrihanDev <shrihankp198725@gmail.com>
2022-09-16 18:13:32 +0530

    update: v3.4.5

--- wsEvents/checkForUpdates.js
+++ wsEvents/checkForUpdates.js
@@ -1,6 +1,6 @@
 const { getDownloadLink } = require('../utils/FileDownloader.js');
 
-const currentVersion = 'v3.4.4';
+const currentVersion = 'v3.4.5';
 
 /**
  * @param {import('ws').WebSocket} ws
