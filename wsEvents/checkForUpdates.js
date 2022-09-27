5dde849c21d9c2fb12a81d9088cfa01087bfaab8
reis <reis@turkeylist.gq>
2022-09-27 06:04:31 +0000

    v3.4.9

--- wsEvents/checkForUpdates.js
+++ wsEvents/checkForUpdates.js
@@ -1,6 +1,6 @@
 const { getDownloadLink } = require('../utils/FileDownloader.js');
 
-const currentVersion = 'v3.4.8';
+const currentVersion = 'v3.4.9';
 
 /**
  * @param {import('ws').WebSocket} ws
