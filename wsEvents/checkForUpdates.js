ae30a54df2b6573feb34fbc8118492ddc78377e3
shrihanDev <shrihankp198725@gmail.com>
2022-10-01 22:17:05 +0530

    chore: bump version
    Signed-off-by: shrihanDev <shrihankp198725@gmail.com>

--- wsEvents/checkForUpdates.js
+++ wsEvents/checkForUpdates.js
@@ -1,6 +1,6 @@
 const { getDownloadLink } = require('../utils/FileDownloader.js');
 
-const currentVersion = 'v3.4.13';
+const currentVersion = 'v3.4.14';
 
 /**
  * @param {import('ws').WebSocket} ws
