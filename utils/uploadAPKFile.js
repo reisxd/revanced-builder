953ab40b84dbd28f329fd848d2024895bbbd0a42
Hmmm <102938585+git-Hmmm@users.noreply.github.com>
2022-12-16 09:20:39 +0530

    Refine Local uploader

--- utils/uploadAPKFile.js
+++ utils/uploadAPKFile.js
@@ -1,36 +1,44 @@
-const AppInfoParser = require('app-info-parser')
+const AppInfoParser = require('app-info-parser');
 const { renameSync } = require('node:fs');
 function uploadAPKFile(req, res, ws) {
-    const file = req.files.apk;
+  const file = req.files.apk;
 
-    file.mv('./revanced/temp.apk', async (err) => {
-        if (err) {
-            for (const websocket of ws) {
-                websocket.send(JSON.stringify({
-                    event: 'error',
-                    error: err
-                }));
-            }
-        }
+  file.mv('./revanced/temp.apk', async (err) => {
+    if (err) {
+      for (const websocket of ws) {
+        websocket.send(
+          JSON.stringify({
+            event: 'error',
+            error: err
+          })
+        );
+      }
+    }
 
-        const app = new AppInfoParser('./revanced/temp.apk');
-        const { package } = await app.parse();
+    const app = new AppInfoParser('./revanced/temp.apk');
+    const resp = await app.parse();
+    const { package } = resp;
+    const { versionName } = resp;
 
-        await renameSync('./revanced/temp.apk', `./revanced/${package}.apk`);
+    await renameSync('./revanced/temp.apk', `./revanced/${package}.apk`);
 
-        global.jarNames.selectedApp = {
-            packageName: package,
-            uploaded: true
-        };
+    global.jarNames.selectedApp = {
+      packageName: package,
+      uploaded: true
+    };
 
-        for (const websocket of ws) {
-            websocket.send(JSON.stringify({
-                event: 'apkUploaded'
-            }));
-        }
-    });
+    for (const websocket of ws) {
+      websocket.send(
+        JSON.stringify({
+          event: 'apkUploaded',
+          package,
+          versionName
+        })
+      );
+    }
+  });
 
-    return res.sendStatus(204);
-};
+  return res.sendStatus(204);
+}
 
 module.exports = uploadAPKFile;
