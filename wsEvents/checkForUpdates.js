5cce19befc0673f2652eaea8c1b1bb2e69ff0bc4
shrihanDev <shrihankp198725@gmail.com>
2022-12-28 15:27:19 +0530

    chore: v3.7.1

--- wsEvents/checkForUpdates.js
+++ wsEvents/checkForUpdates.js
@@ -1,6 +1,6 @@
 const { getDownloadLink } = require('../utils/FileDownloader.js');
 
-const currentVersion = 'v3.7.0';
+const currentVersion = 'v3.7.1';
 
 /**
  * @param {import('ws').WebSocket} ws
