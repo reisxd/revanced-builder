0bd4994c4f46c16d6d6a99be6d150e14145304b5
reis <reis@turkeylist.gq>
2022-12-31 18:25:28 +0000

    chore: bump version

--- wsEvents/checkForUpdates.js
+++ wsEvents/checkForUpdates.js
@@ -1,6 +1,6 @@
 const { getDownloadLink } = require('../utils/FileDownloader.js');
 
-const currentVersion = 'v3.7.1';
+const currentVersion = 'v3.8.0';
 
 /**
  * @param {import('ws').WebSocket} ws
