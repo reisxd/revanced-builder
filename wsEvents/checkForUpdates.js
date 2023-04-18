19fbbe72e6132e72b72794ad99dd3779f29b796a
shrihanDev <shrihankp198725@gmail.com>
2023-04-18 20:01:38 +0530

    chore: bump version to 3.9.1

--- wsEvents/checkForUpdates.js
+++ wsEvents/checkForUpdates.js
@@ -1,6 +1,6 @@
 const { getDownloadLink } = require('../utils/FileDownloader.js');
 
-const currentVersion = 'v3.9.0';
+const currentVersion = 'v3.9.1';
 
 /**
  * @param {import('ws').WebSocket} ws
